f34efca6f136b7aae3198ba57c2eec8788467ab3
Abdolrahman Farshgar <ifarshgar@gmail.com>
2022-01-16 01:19:32 +0100

    Auth0 configurations was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
 import './App.css';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi</h1>
       </header>
+      <div className="login">
+        {!isAuthenticated ? (
+          <div>
+            <h1>Please Login</h1>
+            <LoginButton />
+          </div>
+        ) : (
+          <div>
+            <LogoutButton />
+            <Profile />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="ifarshgar.eu.auth0.com"
+      clientId="ibJWm6JN9VdzNQS00fdtFmDhOqTGlh3u"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
